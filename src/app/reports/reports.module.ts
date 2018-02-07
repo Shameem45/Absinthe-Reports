dd33f3134cab81170e49b6f17aae3cdd5a7871c1
shameem.ahamed@borngroup.com <Shameem1234>
2018-02-07 19:58:47 +0530

    Upadte_on_reportstable_shalini

--- src/app/reports/reports.module.ts
+++ src/app/reports/reports.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReportsComponent } from './reports.component';
+import { Ng2TableModule } from 'ng2-table/ng2-table';
+import { PaginationModule } from 'ng2-bootstrap/ng2-bootstrap'; 
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+   Ng2TableModule,
+   PaginationModule
   ],
   declarations: [ReportsComponent]
 })
